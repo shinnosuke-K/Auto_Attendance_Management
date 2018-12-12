a6405d8a6ef497c4456b630aeee1a9e7573a66ca
shinnosuke-K <shin.050195@gmail.com>
2018-12-12 18:20:25 +0900

    add read_csv()

--- RecordToCSV.py
+++ RecordToCSV.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+import csv
+
+
+def read_csv(filename):
+    f = open(filename, 'r')
+    csv_data = csv.reader(f)
+    data_list = [e for e in csv_data]
+    f.close()
+    return data_list
+
+
+if __name__ == '__main__':
+    data = read_csv('Computer_configuration_theory.csv')
+    print(data)
